3d0721cf1e704bfc8f4e6e9f22839e205738386e
Derrick Plotsky <derrickplotsky@gmail.com>
2018-11-11 19:25:22 -0800

    Remove global acronyms
    We don't use these, so we shouldn't let people think that they could set
    them still.

--- lib/awrence.rb
+++ lib/awrence.rb
@@ -3,12 +3,4 @@ require "awrence/ext/array/to_camel_keys"
 require "awrence/ext/hash/to_camel_keys"
 
 module Awrence
-  class << self
-
-    attr_writer :acronyms
-
-    def acronyms
-      @acronyms ||= {}
-    end
-  end
 end
